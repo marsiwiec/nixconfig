d9a2d08bf190ce15889796ddda37aa5cc1dc7337
marsiwiec <marsiwiec@users.noreply.github.com>
2024-12-23 22:40:23 +0100

    change dbus to broker
    - according to uwsm recommendations:
    https://github.com/Vladimir-csp/uwsm

--- systems/modules/hyprland.nix
+++ systems/modules/hyprland.nix
@@ -10,6 +10,6 @@
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     withUWSM = true;
   };
-
+  services.dbus.implementation = "broker";
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
