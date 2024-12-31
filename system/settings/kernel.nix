dacfea1052c7632288fc81de1fa223068f57728d
marsiwiec <marsiwiec@users.noreply.github.com>
2024-12-31 18:47:24 +0100

    Set plymouth theme. Update.

--- system/settings/kernel.nix
+++ system/settings/kernel.nix
@@ -11,7 +11,15 @@
       kernelPackages = pkgs.linuxPackages_latest;
       loader.systemd-boot.enable = true;
       loader.efi.canTouchEfiVariables = true;
-      plymouth.enable = true;
+      plymouth = {
+        enable = true;
+        theme = lib.mkForce "infinite_seal";
+        themePackages = with pkgs; [
+          (adi1090x-plymouth-themes.override {
+            selected_themes = ["infinite_seal"];
+          })
+        ];
+      };
       consoleLogLevel = 0;
       initrd.verbose = false;
       kernelParams = [
