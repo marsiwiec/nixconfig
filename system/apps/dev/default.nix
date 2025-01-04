aaca722df9c7890c8c99249068a98df7758f7041
marsiwiec <marsiwiec@users.noreply.github.com>
2025-01-04 23:42:46 +0100

    Add devenv

--- system/apps/dev/default.nix
+++ system/apps/dev/default.nix
@@ -1,10 +1,13 @@
 { lib, ... }:
 {
   imports = [
+    ./devenv.nix
     ./R.nix
     ./python.nix
     ./LSPs.nix
   ];
+
+  devenv.enable = lib.mkDefault true;
   R.enable = lib.mkDefault true;
   python.enable = lib.mkDefault true;
   LSPs.enable = lib.mkDefault true;
