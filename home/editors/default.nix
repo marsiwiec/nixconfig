113c49da95c646e3ebbb267f10089ca93b9c2832
marsiwiec <marsiwiec@users.noreply.github.com>
2025-03-18 14:47:14 +0100

    remove vscode config

--- home/editors/default.nix
+++ home/editors/default.nix
@@ -3,12 +3,10 @@
   imports = [
     ./emacs.nix
     ./helix.nix
-    ./vscode.nix
     ./positron.nix
   ];
   # emacs.enable = lib.mkDefault true;
   helix.enable = lib.mkDefault true;
-  vscode.enable = lib.mkDefault true;
   positron.enable = lib.mkDefault true;
 
 }
