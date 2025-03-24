59408b2593ba25311a9f04f949f050c0050eccb7
marsiwiec <marsiwiec@users.noreply.github.com>
2025-03-24 06:20:01 +0100

    Override Positron to newest version.

--- home/editors/positron.nix
+++ home/editors/positron.nix
@@ -4,13 +4,21 @@
   pkgs,
   ...
 }:
+let
+  positron = pkgs.positron-bin.overrideAttrs (
+    finalAttrs: previousAttrs: {
+      src = pkgs.fetchurl {
+        url = "https://cdn.posit.co/positron/dailies/deb/x86_64/Positron-2025.04.0-109-x64.deb";
+        hash = "sha256-OZtH88Krm3x4Gw/iXXud6kx7OfwHOmD1eRitpgI+XBM=";
+      };
+    }
+  );
+in
 {
   options = {
     positron.enable = lib.mkEnableOption "positron IDE";
   };
   config = lib.mkIf config.positron.enable {
-    home.packages = with pkgs; [
-      positron-bin
-    ];
+    home.packages = [ positron ];
   };
 }
