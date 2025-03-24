2062e1eaab9a3de3cb857964bc390fe3e6163d4d
marsiwiec <marsiwiec@users.noreply.github.com>
2025-03-24 20:43:30 +0100

    Another Positron update.

--- home/editors/positron.nix
+++ home/editors/positron.nix
@@ -8,8 +8,8 @@ let
   positron = pkgs.positron-bin.overrideAttrs (
     finalAttrs: previousAttrs: {
       src = pkgs.fetchurl {
-        url = "https://cdn.posit.co/positron/dailies/deb/x86_64/Positron-2025.04.0-109-x64.deb";
-        hash = "sha256-OZtH88Krm3x4Gw/iXXud6kx7OfwHOmD1eRitpgI+XBM=";
+        url = "https://cdn.posit.co/positron/dailies/deb/x86_64/Positron-2025.04.0-173-x64.deb";
+        hash = "sha256-ap9UcZ++cLocex67v8tRf56AhRCbJG/jdGIIoLxiY/c=";
       };
     }
   );
