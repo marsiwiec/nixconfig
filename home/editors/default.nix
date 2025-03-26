4fceb02b407545cd8d710b699a864c0183a34c7f
marsiwiec <marsiwiec@users.noreply.github.com>
2025-03-26 20:17:10 +0100

    Remove global RStudio config.

--- home/editors/default.nix
+++ home/editors/default.nix
@@ -4,11 +4,9 @@
     ./emacs.nix
     ./helix.nix
     ./positron.nix
-    ./rstudio.nix
   ];
   # emacs.enable = lib.mkDefault true;
   helix.enable = lib.mkDefault true;
   positron.enable = lib.mkDefault true;
-  rstudio.enable = lib.mkDefault true;
 
 }
