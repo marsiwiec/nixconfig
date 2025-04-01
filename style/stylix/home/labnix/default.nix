e8b3323e265355a71b5eaa00facaa57b501ce5e7
marsiwiec <marsiwiec@users.noreply.github.com>
2025-04-01 08:47:41 +0200

    Update wallpaper on labnix

--- style/stylix/home/labnix/default.nix
+++ style/stylix/home/labnix/default.nix
@@ -2,8 +2,8 @@
 {
   stylix = {
     image = pkgs.fetchurl {
-      url = "https://github.com/marsiwiec/nixconfig/blob/main/style/wallpapers/wallpaper.png?raw=true";
-      sha256 = "sha256-xm9MgisDK+3dwsL3SpFHP9D6Z3GWCyhNNKjm5KYTx9k=";
+      url = "https://github.com/marsiwiec/nixconfig/blob/main/style/wallpapers/cheshire_cat.png?raw=true";
+      sha256 = "sha256-XQkYxhbnNGs98f/4rm1yGSF0Nse5koZ7bm94cfwF890=";
     };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
   };
