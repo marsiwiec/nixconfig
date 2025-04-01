0fc4faafde35b5ef614250bde610c560e1fbb3cd
marsiwiec <marsiwiec@users.noreply.github.com>
2025-04-01 21:00:37 +0200

    add mimeapps.list for firefox

--- home.nix
+++ home.nix
@@ -17,6 +17,22 @@
     VISUAL = "hx";
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+    };
+  };
+
   home.shell.enableZshIntegration = true;
 
   # Let Home Manager install and manage itself.
