cb8811bca14d6aa0ae76a392b24e7b7e6647887f
marsiwiec <marsiwiec@users.noreply.github.com>
2025-09-03 21:38:29 +0200

    Switch stylix theme.

--- style/stylix/system/labnix/default.nix
+++ style/stylix/system/labnix/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   stylix = {
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
     image = ../../../wallpapers/star_wars.png;
 
   };
