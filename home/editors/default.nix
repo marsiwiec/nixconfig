9a4734d10015686de5cf2a3dd68fc904ba7237da
marsiwiec <marsiwiec@users.noreply.github.com>
2025-09-21 11:21:26 +0200

    Remove zed editor, don't use it at all.

--- home/editors/default.nix
+++ home/editors/default.nix
@@ -4,11 +4,9 @@
     ./emacs
     ./helix.nix
     ./positron.nix
-    ./zed.nix
   ];
   emacs.enable = lib.mkDefault true;
   helix.enable = lib.mkDefault true;
   positron.enable = lib.mkDefault true;
-  zed.enable = lib.mkDefault true;
 
 }
