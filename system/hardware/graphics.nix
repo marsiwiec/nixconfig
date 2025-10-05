1a1725ffd55e18ee4838ee17884970d825cde33f
marsiwiec <marsiwiec@users.noreply.github.com>
2025-10-05 19:29:37 +0200

    Remove unnecessary comment

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -9,7 +9,6 @@
     graphics.enable = lib.mkEnableOption "enable basic graphics support";
   };
   config = lib.mkIf config.graphics.enable {
-    # Enable the X11 windowing system.
     services = {
       xserver.enable = true;
     };
