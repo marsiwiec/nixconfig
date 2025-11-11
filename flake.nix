76052992afe8e79c073bed6740d929b3a6cc2d28
marsiwiec <marsiwiec@users.noreply.github.com>
2025-11-11 12:44:48 +0100

    Set up Dank Material Shell

--- flake.nix
+++ flake.nix
@@ -42,6 +42,23 @@
       inputs.home-manager.follows = "home-manager";
     };
 
+    dgop = {
+      url = "github:AvengeMedia/dgop";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    dms-cli = {
+      url = "github:AvengeMedia/danklinux";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    dankMaterialShell = {
+      url = "github:AvengeMedia/DankMaterialShell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.dgop.follows = "dgop";
+      inputs.dms-cli.follows = "dms-cli";
+    };
+
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/home/nixos/common/desktop/default.nix
+++ modules/home/nixos/common/desktop/default.nix
@@ -8,11 +8,13 @@
     ./waybar.nix
     ./hypridle.nix
     ./swayidle.nix
+    ./dank_material_shell.nix
   ];
   niri.enable = lib.mkDefault true;
   # hyprland.enable = lib.mkDefault true;
   # plasma-manager.enable = lib.mkDefault true;
-  waybar.enable = lib.mkDefault true;
+  # waybar.enable = lib.mkDefault true;
   # hypridle.enable = lib.mkDefault true;
   swayidle.enable = lib.mkDefault true;
+  dms.enable = lib.mkDefault true;
 }
