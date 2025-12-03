0010b08906ba368670cf1430eb085489bc30ad08
marsiwiec <marsiwiec@users.noreply.github.com>
2025-12-03 20:52:54 +0100

    stylix colors

--- modules/home/nixos/common/desktop/noctalia.nix
+++ modules/home/nixos/common/desktop/noctalia.nix
@@ -14,9 +14,25 @@
   };
 
   config = lib.mkIf config.noctalia.enable {
-    programs.noctalia-shell = {
+    programs.noctalia-shell = with config.lib.stylix.colors; {
       enable = true;
       systemd.enable = true;
+      colors = {
+        mError = "#${base08}";
+        mOnError = "#${base00}";
+        mOnPrimary = "#${base00}";
+        mOnSecondary = "#${base00}";
+        mOnSurface = "#${base04}";
+        mOnSurfaceVariant = "#${base04}";
+        mOnTertiary = "#${base00}";
+        mOutline = "#${base02}";
+        mPrimary = "#${base0B}";
+        mSecondary = "#${base0A}";
+        mShadow = "#${base00}";
+        mSurface = "#${base00}";
+        mSurfaceVariant = "#${base01}";
+        mTertiary = "#${base0D}";
+      };
     };
   };
 }
