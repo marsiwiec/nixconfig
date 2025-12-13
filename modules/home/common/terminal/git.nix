b53a54ec49f58fe116e7315cf962b1cceeba4e6e
marsiwiec <marsiwiec@users.noreply.github.com>
2025-12-13 14:30:15 +0100

    Remove gh-cli auth

--- modules/home/common/terminal/git.nix
+++ modules/home/common/terminal/git.nix
@@ -17,21 +17,9 @@
           email = "marsiwiec@users.noreply.github.com";
         };
         init.defaultBranch = "main";
-        credential = {
-          "https://github.com" = {
-            helper = "!gh auth git-credential";
-          };
-          "https://gist.github.com" = {
-            helper = "!gh auth git-credential";
-          };
-        };
       };
     };
     programs = {
-      gh = {
-        enable = true;
-        gitCredentialHelper.enable = false;
-      };
       lazygit.enable = true;
     };
   };
