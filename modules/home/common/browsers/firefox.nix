af5c61b4a763e3cf8fcef19473252253a6b9760d
marsiwiec <marsiwiec@users.noreply.github.com>
2026-01-28 09:37:33 +0100

    Move firefox policies and settings to home-manager module, remove nixos firefox module

--- modules/home/common/browsers/firefox.nix
+++ modules/home/common/browsers/firefox.nix
@@ -16,6 +16,26 @@
           id = 0;
           name = "default";
           path = "default";
+          settings = {
+            "browser.aboutConfig.showWarning" = false;
+            "browser.tabs.loadInBackground" = true;
+            "media.ffmpeg.vaapi.enabled" = true;
+            "extensions.autoDisableScopes" = 0;
+            "browser.ml.chat.enabled" = false;
+            "browser.tabs.groups.smart.enabled" = false;
+          };
+        };
+      };
+      policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableFirefoxScreenshots = true;
+        HardwareAcceleration = true;
+        EnableTrackingProtection = {
+          Value = true;
+          Cryptomining = true;
+          Fingerprinting = true;
         };
       };
     };
--- modules/system/nixos/apps/default.nix
+++ modules/system/nixos/apps/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ./utils.nix
     ./thunar.nix
-    ./firefox.nix
     ./sunshine.nix
     ./distrobox.nix
     ./lan-mouse.nix
@@ -18,7 +17,6 @@
   nh.enable = lib.mkDefault true;
   utils.enable = lib.mkDefault true;
   thunar.enable = lib.mkDefault true;
-  firefox.enable = lib.mkDefault true;
   sunshine.enable = lib.mkDefault true;
   distrobox.enable = lib.mkDefault true;
   ollama.enable = lib.mkDefault true;
