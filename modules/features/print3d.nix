9c0a14ebefae64ed48bf63889de4136884b22e6d
marsiwiec <marsiwiec@users.noreply.github.com>
2026-02-26 09:15:49 +0100

    update icons

--- modules/features/print3d.nix
+++ modules/features/print3d.nix
@@ -1,6 +1,6 @@
 {
   flake.modules.homeManager.print3d =
-    { pkgs, ... }:
+    { osConfig, pkgs, ... }:
     {
       home.packages = with pkgs; [
         bambu-studio
@@ -12,10 +12,7 @@
           exec = "bambu-studio";
           terminal = false;
           type = "Application";
-          icon = builtins.fetchurl {
-            url = "https://github.com/bambulab/BambuStudio/blob/b97b6475393eaa249e1a7f2c26f50b3d3d1a1ee4/resources/images/BambuStudio_192px.png";
-            sha256 = "114qnz0i6qjm3zws31yw237kkcq969gw8ymd3jab24wsxjd6mv7r";
-          };
+          icon = "${osConfig.systemConstants.iconDir}/BambuStudio_192px.png";
         })
       ];
     };
