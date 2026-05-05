d3e950d4375e8bf42ea36592a1046a1a4c5c1dd8
marsiwiec <marsiwiec@users.noreply.github.com>
2026-05-05 14:28:49 +0200

    Add jq to utils

--- modules/features/utils.nix
+++ modules/features/utils.nix
@@ -1,19 +1,30 @@
+{ inputs, ... }:
 {
-  flake.modules.nixos.utils =
-    { pkgs, ... }:
-    {
-      environment.localBinInPath = true;
-      environment.systemPackages = with pkgs; [
-        neovim
-        pciutils
-        wget
-        fastfetch
-        ripgrep
-        tree
-        fzf
-        dysk
-        inotify-tools
-        gparted
-      ];
+  flake.modules = {
+    nixos.utils =
+      { pkgs, ... }:
+      {
+        home-manager.sharedModules = [
+          inputs.self.modules.homeManager.utils
+        ];
+        environment.localBinInPath = true;
+        environment.systemPackages = with pkgs; [
+          neovim
+          pciutils
+          wget
+          fastfetch
+          ripgrep
+          tree
+          dysk
+          inotify-tools
+          gparted
+        ];
+      };
+    homeManager.utils = {
+      programs = {
+        jq.enable = true;
+        fzf.enable = true;
+      };
     };
+  };
 }
