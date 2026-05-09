4e22411c6e99f6550fea1b7ae4a3187fbd908c44
marsiwiec <marsiwiec@users.noreply.github.com>
2026-05-09 22:37:07 +0200

    add override for positron update.

--- modules/features/development/ide/positron.nix
+++ modules/features/development/ide/positron.nix
@@ -2,6 +2,18 @@
   flake.modules.homeManager.positron =
     { pkgs, ... }:
     {
-      home.packages = with pkgs; [ positron-bin ];
+      home.packages = with pkgs; [
+        (positron-bin.overrideAttrs (old: {
+          version = "2026.05.1-2";
+          src = pkgs.fetchurl {
+            url = "https://cdn.posit.co/positron/releases/deb/x86_64/Positron-2026.05.1-2-x64.deb";
+            hash = "sha256-XBjC9hxOr6ioDpcZ4MT+hFOaxTXl+Xs331hyEdhx+oY=";
+          };
+          buildInputs = old.buildInputs ++ [
+            pkgs.libsecret
+            pkgs.webkitgtk_4_1
+          ];
+        }))
+      ];
     };
 }
