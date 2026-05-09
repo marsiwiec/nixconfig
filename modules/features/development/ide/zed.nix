ffb5afcb6d404760743f07668e30e186172a7173
marsiwiec <marsiwiec@users.noreply.github.com>
2026-05-09 12:14:29 +0200

    Wrap zeditor to force the Vulkan loader to select radv, avoiding slow GPU probing on startup

--- modules/features/development/ide/zed.nix
+++ modules/features/development/ide/zed.nix
@@ -17,9 +17,22 @@
 
   flake.modules.homeManager.zed =
     { pkgs, ... }:
+    let
+      # Wrap zeditor to force the Vulkan loader to select RADV, avoiding slow GPU probing on startup.
+      zed-editor-wrapped = pkgs.symlinkJoin {
+        name = "zed-editor-wrapped";
+        paths = [ pkgs.unstable.zed-editor ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          rm $out/bin/zeditor
+          makeWrapper ${pkgs.unstable.zed-editor}/bin/zeditor $out/bin/zeditor \
+            --set VK_LOADER_DRIVERS_SELECT "*radv*"
+        '';
+      };
+    in
     {
-      home.packages = with pkgs.unstable; [
-        zed-editor
+      home.packages = [
+        zed-editor-wrapped
       ];
     };
 }
