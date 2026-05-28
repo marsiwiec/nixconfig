67b92088ff0eaf0e7a2c8fa5448ef01804a23aa4
marsiwiec <marsiwiec@users.noreply.github.com>
2026-05-28 20:35:02 +0200

    update Positron version

--- modules/features/development/ide/positron.nix
+++ modules/features/development/ide/positron.nix
@@ -6,8 +6,8 @@
         (positron-bin.overrideAttrs (old: {
           version = "2026.05.1-2";
           src = pkgs.fetchurl {
-            url = "https://cdn.posit.co/positron/releases/deb/x86_64/Positron-2026.05.1-2-x64.deb";
-            hash = "sha256-XBjC9hxOr6ioDpcZ4MT+hFOaxTXl+Xs331hyEdhx+oY=";
+            url = "https://cdn.posit.co/positron/releases/deb/x86_64/Positron-2026.05.2-3-x64.deb";
+            hash = "sha256-oCW3E8dZNksaKWdRZOeqXwEIgX0oCfe6VvTxBe18jYc=";
           };
           buildInputs = old.buildInputs ++ [
             pkgs.libsecret
