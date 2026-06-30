784b08e35fc0647d59af0f54de9b6374a11d86cf
marsiwiec <marsiwiec@users.noreply.github.com>
2026-06-30 12:57:11 +0200

    Use positron-bin from pkgs-unstable

--- modules/features/dev/ide/positron.nix
+++ modules/features/dev/ide/positron.nix
@@ -2,18 +2,8 @@
   flake.modules.homeManager.positron =
     { pkgs, ... }:
     {
-      home.packages = with pkgs; [
-        (positron-bin.overrideAttrs (old: {
-          version = "2026.06.0-211";
-          src = pkgs.fetchurl {
-            url = "https://cdn.posit.co/positron/releases/deb/x86_64/Positron-2026.06.0-211-x64.deb";
-            hash = "sha256-YvnweVTKAvxZTR5/FY1VWt03Gx4LFa2faL+Z0AYCtpY=";
-          };
-          buildInputs = old.buildInputs ++ [
-            pkgs.libsecret
-            pkgs.webkitgtk_4_1
-          ];
-        }))
+      home.packages = with pkgs.unstable; [
+        positron-bin
       ];
     };
 }
