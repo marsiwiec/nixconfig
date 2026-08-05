652d0401c382d6332a33224a50cd6fc515cb205b
marsiwiec <marsiwiec@users.noreply.github.com>
2026-08-05 19:42:20 +0200

    Add pi agent

--- modules/features/dev/ai.nix
+++ modules/features/dev/ai.nix
@@ -2,9 +2,9 @@
   flake.modules.homeManager.ai =
     { pkgs, ... }:
     {
-      programs.opencode = {
-        enable = true;
-        package = pkgs.unstable.opencode;
+      programs = {
+        opencode.enable = true;
+        pi-coding-agent.enable = true;
       };
       home.packages = with pkgs; [
         nodejs
