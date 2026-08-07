820bc4cddbeecb76f7cc7908449daa5f6560554b
marsiwiec <marsiwiec@users.noreply.github.com>
2026-08-07 22:16:56 +0200

    Force no ai in firefox

--- modules/features/browsers/firefox.nix
+++ modules/features/browsers/firefox.nix
@@ -8,12 +8,26 @@
           name = "default";
           path = "default";
           settings = {
+            # these flags actualy disable the AI features
+            "browser.ml.chat.enabled" = false;
+            "browser.ml.chat.page" = false;
+            "browser.ml.linkPreview.enabled" = false;
+            "browser.tabs.groups.smart.enabled" = false;
+            "browser.tabs.groups.smart.userEnabled" = false;
+            "extensions.ml.enabled" = false;
+            "pdfjs.enableAltText" = false;
+
+            # these flags change the visual UI on the AI panel
+            "browser.ai.control.default" = "blocked";
+            "browser.ai.control.linkPreviewKeyPoints" = "blocked";
+            "browser.ai.control.pdfjsAltText" = "blocked";
+            "browser.ai.control.sidebarChatbot" = "blocked";
+            "browser.ai.control.smartTabGroups" = "blocked";
+
             "browser.aboutConfig.showWarning" = false;
             "browser.tabs.loadInBackground" = true;
             "media.ffmpeg.vaapi.enabled" = true;
             "extensions.autoDisableScopes" = 0;
-            "browser.ml.chat.enabled" = false;
-            "browser.tabs.groups.smart.enabled" = false;
             "gnomeTheme.tabsAsHeaderbar" = true;
           };
         };
