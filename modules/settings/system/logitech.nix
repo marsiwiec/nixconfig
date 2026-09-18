3f85dc89976eae61b7e710ca29c464c4e3ce00cc
marsiwiec <marsiwiec@users.noreply.github.com>
2026-09-18 18:49:40 +0200

    Use newest src for logitech mouse stuff (libratbag and piper)

--- modules/settings/system/logitech.nix
+++ modules/settings/system/logitech.nix
@@ -2,10 +2,20 @@
   flake.modules.nixos.logitech =
     { pkgs, ... }:
     {
-      services.ratbagd.enable = true;
-
-      environment.systemPackages = with pkgs; [
-        piper
+      services.ratbagd = {
+        enable = true;
+        package = pkgs.libratbag.overrideAttrs (old: {
+          version = "0-unstable-2026-08-18";
+          src = pkgs.fetchFromGitHub {
+            owner = "libratbag";
+            repo = "libratbag";
+            rev = "b8d4d3ca1f4d6b23c664ffee2888b8eb669bee21";
+            sha256 = "sha256-8V/LIki/tI/9Wi6kuFJp6k1p+moMh8Gc8RNP1BUlZO8=";
+          };
+        });
+      };
+      services.flatpak.packages = [
+        "org.freedesktop.Piper"
       ];
     };
 }
